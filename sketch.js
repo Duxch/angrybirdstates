27d2b7d7ef80a63a1e2a9152b795f1c272e6b4be
Duxch <15duxch321@gmail.com>
2021-05-23 19:50:17 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -17,9 +17,8 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
-    box1 = new Box(700,320,70,70);
+   platform = new Ground(180,305,300,170);
+     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
@@ -60,5 +59,5 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+   platform.display();
 }
